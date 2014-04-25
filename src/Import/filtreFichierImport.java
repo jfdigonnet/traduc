396490ee6101e15de740a22bf4d6b3d37bd3c80c
digonnet <digonnet@ae6374b2-96da-4077-91ad-7465d3891a8b>
2014-04-25 09:23:51 +0000

    Ajout mots anglais

--- src/Import/filtreFichierImport.java
+++ src/Import/filtreFichierImport.java
@@ -3,25 +3,25 @@ package Import;
 import java.io.File;
 import javax.swing.filechooser.*;
 
-import utilitaires.Utils;
- 
 public class filtreFichierImport extends FileFilter
 {
     public boolean accept(File f)
     {
         if (f.isDirectory()) {
             return true;
+        } else {
+        	return (f.getName().toLowerCase().endsWith(".csv") || f.getName().toLowerCase().endsWith(".txt"));
         }
-        String extension = Utils.getExtension(f);
-        if (extension != null) {
-        	if (extension.equals(Utils.csv) || extension.equals(Utils.CSV) || extension.equals(Utils.TXT) || extension.equals(Utils.txt))
-            {
-                    return true;
-            } else {
-                return false;
-            }
-        } 
-        return false;    
+//        String extension = Utils.getExtension(f);
+//        if (extension != null) {
+//        	if (extension.equals(Utils.csv) || extension.equals(Utils.CSV) || extension.equals(Utils.TXT) || extension.equals(Utils.txt))
+//            {
+//                    return true;
+//            } else {
+//                return false;
+//            }
+//        } 
+//        return false;    
     }
     public String getDescription()
     {
--- src/sauverestau/filtreFichierZIP.java
+++ src/sauverestau/filtreFichierZIP.java
@@ -12,16 +12,18 @@ public class filtreFichierZIP extends FileFilter {
 	public boolean accept(File f) {
         if (f.isDirectory()) {
             return true;
+        } else {
+        	return (f.getName().toLowerCase().endsWith(".zip"));
         }
-        String extension = Utils.getExtension(f);
-        if (extension != null) {
-            if (extension.equals(Utils.zip) ||  extension.equals(Utils.ZIP)) {
-                    return true;
-            } else {
-                return false;
-            }
-        } 
-        return false;    
+//        String extension = Utils.getExtension(f);
+//        if (extension != null) {
+//            if (extension.equals(Utils.zip) ||  extension.equals(Utils.ZIP)) {
+//                    return true;
+//            } else {
+//                return false;
+//            }
+//        } 
+//        return false;    
 	}
 
 	@Override
